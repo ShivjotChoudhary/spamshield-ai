67e38be76b87f76eeb6b87a60930b9ddc812649c
Shivjot Choudhary <shivjotchaudhary0@gmail.com>
2026-04-01 15:53:05 +0530

    updated project

--- app.py
+++ app.py
@@ -78,5 +78,7 @@ def predict():
 
 
 # 🔹 Run app
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000)
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
